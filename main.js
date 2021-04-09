8ded6a35578effffa0ec82a76c6410ca10deeb53
variables_constantes_tipo_de_datos_26_03_2021 <81273279+AndresGelvezS@users.noreply.github.com>
2021-04-08 22:39:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Decalaracion de Variables-------
+
+let NombreApellido;
+let Edad;
+let Alumno;
+
+
+// Asignacion de Variables--------
+
+NombreApellido= `Andres Gelvez`;
+Edad=37;
+Alumno=true;
+
+// Imprimir--------
+console.log(NombreApellido, typeof(NombreApellido));
+console.log(Edad, typeof(Edad));
+console.log(Alumno, typeof(Alumno));
+
